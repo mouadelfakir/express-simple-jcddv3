be23136d5e51a4385bc20a1c2e26e8981d0b1972
mouadelfakir <mouad.fkr@gmail.com>
2023-05-11 10:27:21 +0200

    3 files modified

--- index.js
+++ index.js
@@ -3,11 +3,31 @@ const app = express();
 const port = 3010;
 const path = require('path');
 const cors = require('cors');
+const https = require('https');
+const axios = require('axios');
+
 const orders = require('./orders.json');
 
 app.use(cors());
 
 app.get('/getOrdersWithoutImages', (req, res) => {
+  res.send(orders);
+});
+
+app.get('/getOrdersWithImages', (req, res) => {
+  const endpoints = [];
+  orders.forEach((order) =>
+    endpoints.push(
+      'https://express-simple-rpwg.onrender.com/getImageUrl?key=' + order.key
+    )
+  );
+
+  axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((data) => {
+    data.forEach((image, index) => {
+      orders[index]['imageUrl'] = image.data.imageUrl;
+    });
+  });
+
   res.send(orders);
 });
 
